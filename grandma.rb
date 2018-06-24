b216d5fa1f493746e3420e9d444a70c0e2b04787
Christopher Beltran <christod.beltran@gmail.com>
2018-06-24 20:45:21 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
     
-  elseif phrase.upcase?
+  elseif phrase != phrase.downcase
     return "NO, NOT SINCE 1938!"
   
   else
